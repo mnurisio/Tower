93b73bee37af5cbf5efa83af8b90257b78a9cc66
Mike Nurisio <michaelnurisio@gmail.com>
2025-03-13 11:22:37 -0600

    styled comment card

--- client/src/components/CommentCard.vue
+++ client/src/components/CommentCard.vue
@@ -29,32 +29,47 @@ async function deleteComment(commentId) {
 
 
 <template>
-    <div class="row mb-2 align-items-center p-2 m-1 comment">
-        <img class="commentImg col-md-2 col-3 px-md-4" :src="comment.creator.picture" :alt="comment.creator.name">
-        <span class="ms-md-3 ms-0 col-12 col-md-9">
-            <h6 class="mt-md-2 mt-3">{{ comment.creator.name }}</h6>
-            <div class="">
-                <p class="text-break">{{ comment.body }}</p>
+    <div class="row m-2 pt-2 align-items-center comment">
+        <div class="col-2 p-0">
+            <img class="commentImg mx-auto d-block" :src="comment.creator.picture" :alt="comment.creator.name">
+        </div>
+        <div class="col-10 ps-0">
+            <h6 class="mt-md-2 mt-3 kanit-medium">{{ comment.creator.name }}</h6>
+            <div>
+                <p class="text-break kanit-light mb-1">{{ comment.body }}</p>
             </div>
-        </span>
+        </div>
         <div v-if="comment.creatorId == account?.id" class="text-end">
-            <button @click="deleteComment(comment.id)" class="btn btn-danger">Delete</button>
+            <button @click="deleteComment(comment.id)" class="btn my-2">Remove</button>
         </div>
     </div>
 </template>
 
 
 <style lang="scss" scoped>
-
 .commentImg {
     height: 6dvh;
     aspect-ratio: 1/1;
     border-radius: 50%;
+    object-fit: cover;
+    object-position: center;
 }
 
-.comment{
+.comment {
     background-color: rgba(255, 255, 255);
     box-shadow: 1px 8px 13px -3px rgba(0, 0, 0, 0.27);
 }
 
+button {
+    background-color: #DA4C0F;
+    color: #F8F6FF;
+}
+
+div h6{
+    color: #696969;
+}
+
+div p{
+    color: #696969;
+}
 </style>
