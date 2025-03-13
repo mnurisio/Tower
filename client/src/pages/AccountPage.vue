437b96596f37f262658897300de1ca3b91522dbf
Mike Nurisio <michaelnurisio@gmail.com>
2025-03-13 11:41:06 -0600

    styled account page

--- client/src/pages/AccountPage.vue
+++ client/src/pages/AccountPage.vue
@@ -36,22 +36,24 @@ async function refundTicket(ticketId) {
 </script>
 
 <template>
-  <div class="about text-center">
-    <div v-if="account">
-      <h1 class="my-2">Welcome {{ account.name }}</h1>
-      <img class="rounded my-2" :src="account.picture" alt="" />
+  <div class="about text-center container">
+    <div v-if="account" class="row justify-content-start align-items-center">
+      <div class="col-md-2 col-3">
+        <img class="my-2" :src="account.picture" :alt="account.name" />
+      </div>
+      <div class="col-9 text-start">
+        <h1 class="my-2">{{ account.name }}</h1>
+      </div>
     </div>
-    <div class="container">
+    <div class="container mt-5">
       <section class="row">
         <div class="text-md-start text-center my-2">
           <h3>Your Upcoming Events</h3>
-          <hr>
         </div>
-        <div v-for="myTicket in myTickets" :key="myTicket.id" class="col-md-3 col-12 text-start">
+        <div v-for="myTicket in myTickets" :key="myTicket.id" class="col-md-3 col-12 text-start my-ticket">
           <EventCard :event="myTicket.event" />
           <div class="text-md-end text-center">
-            <button @click="refundTicket(myTicket.id)" class="btn btn-danger my-1">Refund Ticket <i
-                class="mdi mdi-arrow-up-thick"></i></button>
+            <button @click="refundTicket(myTicket.id)" class="btn">Unattend</button>
           </div>
         </div>
       </section>
@@ -63,5 +65,39 @@ async function refundTicket(ticketId) {
 <style scoped lang="scss">
 img {
   max-width: 100px;
+  aspect-ratio: 1/1;
+  border-radius: 50%;
+  box-shadow: 1px 2px 13px 1px rgba(0, 0, 0, 0.27);
 }
+
+.my-ticket{
+  position: relative;
+}
+
+@media(min-width: 768px){
+  button{
+    background-color: #5044DE;
+    color: #F8F6FF;
+    position: absolute;
+    bottom: calc(40%);
+    right: calc(8%);
+  }
+}
+
+@media(max-width: 768px){
+  button{
+    background-color: #5044DE;
+    color: #F8F6FF;
+    position: absolute;
+    bottom: calc(40%);
+    right: calc(5%);
+  }
+}
+
+
+button:hover{
+  background-color: #5044DE;
+  color: #F8F6FF;
+}
+
 </style>
